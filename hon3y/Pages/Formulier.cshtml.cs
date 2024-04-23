3c019868aeb6f8e0ad57e9f145f6758bc1aee611
Sushi <31491170+0974201@users.noreply.github.com>
2024-04-23 15:06:54 +0200

    added new pages, small changes to header

---
+++ hon3y/Pages/Formulier.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace hon3y.Pages
+{
+    public class FormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
