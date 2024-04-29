3de8b79f02e6180876cc508c5801471aeec04ef8
Sushi <31491170+0974201@users.noreply.github.com>
2024-04-29 16:14:02 +0200

    #2 added db model

---
+++ hon3y/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace hon3y.Models
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ hon3y/Models/LoginContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace hon3y.Models
+{
+    public class LoginContext : DbContext
+    {
+        public LoginContext(DbContextOptions options) : base( options )
+        {
+
+        }
+    }
+}
