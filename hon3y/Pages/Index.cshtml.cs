0d1cc6f97e1fdd4a5f8326476ba92b993d36a1c4
Sushi <31491170+0974201@users.noreply.github.com>
2024-04-30 17:01:16 +0200

    #2 deleted getclient.cs

--- hon3y/Pages/Index.cshtml.cs
+++ hon3y/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using hon3y.Services;
 
@@ -7,17 +8,23 @@ namespace hon3y.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        public string? Ip { get; set; }
+        public string Ip;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
+        private string GetIp()
+        {
+            Ip = HttpContext.Connection.RemoteIpAddress.ToString();
+
+            return Ip;
+        }
+
         public void OnGet()
         {
-            //Ip = GetClientIp;
-            Console.WriteLine(Ip);
+            Console.WriteLine(GetIp());
         }
     }
 }
