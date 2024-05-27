1e74c6da4dd8be5f96ba9b4472e2f6b89244db50
Vidya <31491170+0974201@users.noreply.github.com>
2024-05-27 12:21:19 +0200

    #2 added DataService.cs

---
+++ hon3y/Services/DataService.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.Data.Sqlite;
+
+namespace hon3y.Services
+{
+    public class DataService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        private readonly string _connectionString2; //electric boogaloo
+
+        public DataService(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+            _connectionString2 = _configuration.GetConnectionString("NotDefaultConnection");
+        }
+
+        public void InsertData()
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+            }
+        }
+    }
+}
